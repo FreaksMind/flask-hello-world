14d8b2027a2da2fe217832dc1f023296f11913c7
FreaksMind <31421210+FreaksMind@users.noreply.github.com>
2025-05-27 20:03:24 +0300

    updated index.py

--- api/index.py
+++ api/index.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/api/post_question", methods=["POST"])
+@app.route("/api/post_question", methods=["GET"])
 def test():
     return jsonify({"status": "success", "message": "Integrate Flask Framework with Next.js"})
 
